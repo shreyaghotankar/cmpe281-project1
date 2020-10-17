d0c7810dd059febe4da37f4486b79f87941b7ca0
shreyaghotankar <shreya.ghotankar@sjsu.edu>
2020-10-17 00:15:23 -0700

    Adding Okta Login
    adding okta as Identity provider

--- src/main/java/com/whippystore/storage/controller/HomeController.java
+++ src/main/java/com/whippystore/storage/controller/HomeController.java
@@ -30,29 +30,26 @@ public class HomeController {
     public String getHomePage(Model model, Authentication authentication) {
         LOG.info("Welcome here !!!");
         if (authentication != null && authentication.isAuthenticated()) {
-            if (authentication.getAuthorities().stream().anyMatch(a -> a.getAuthority().equals("ADMIN"))) {
-                model.addAttribute("secretMessage", "Admin message is s3crEt");
-            } else {
-                List<UserUploadFileRecord> records = dao.getUserUploadRecordByEmail( ((DefaultOidcUser) authentication.getPrincipal()).getEmail());
-                List<UserUploadFileRecordDTO> dtos = new ArrayList<>();
-             if(records != null) {
-                for (UserUploadFileRecord record :  records) {
-                    UserUploadFileRecordDTO dto = new UserUploadFileRecordDTO();
+        	List<UserUploadFileRecord> records = dao.getUserUploadRecordByEmail( ((DefaultOidcUser) authentication.getPrincipal()).getEmail());
+            List<UserUploadFileRecordDTO> dtos = new ArrayList<>();
+         if(records != null) {
+            for (UserUploadFileRecord record :  records) {
+                UserUploadFileRecordDTO dto = new UserUploadFileRecordDTO();
 
-                    dto.setFilename(record.getFilename());
-                    dto.setId(record.getId());
-                    dto.setDescription(record.getDescription());
-                    dto.setFirstname(record.getFirstname());
-                    dto.setLastname(record.getLastname());
-                    dto.setCreatedTime(record.getCreatedTime());
-                    dto.setUpdatedTime(record.getUpdatedTime());
-                    dto.setPath(record.getPath());
-                    dtos.add(dto);
-                }
-             }
-
-                model.addAttribute("records", dtos);
+                dto.setFilename(record.getFilename());
+                dto.setId(record.getId());
+                dto.setDescription(record.getDescription());
+                dto.setFirstname(record.getFirstname());
+                dto.setLastname(record.getLastname());
+                dto.setCreatedTime(record.getCreatedTime());
+                dto.setUpdatedTime(record.getUpdatedTime());
+                dto.setPath(record.getPath());
+                dtos.add(dto);
             }
+         }
+
+            model.addAttribute("records", dtos); 
+           
         }
 
         return "home";
--- src/main/java/com/whippystore/storage/security/SecurityConfig.java
+++ src/main/java/com/whippystore/storage/security/SecurityConfig.java
@@ -1,9 +1,10 @@
-package com.whippystore.storage.cognito;
+package com.whippystore.storage.security;
 
 import java.net.URI;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.oauth2.client.oidc.web.logout.OidcClientInitiatedLogoutSuccessHandler;
@@ -13,14 +14,15 @@ import org.springframework.security.oauth2.client.web.OAuth2LoginAuthenticationF
 import com.whippystore.storage.customfilter.OAuth2AuthenticationFilter;
 
 @Configuration
+@EnableGlobalMethodSecurity(prePostEnabled = true) 
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	
 	@Autowired
-    ClientRegistrationRepository clientRegistrationRepository;
+    private ClientRegistrationRepository clientRegistrationRepository;
 
     OidcClientInitiatedLogoutSuccessHandler oidcLogoutSuccessHandler() {
         OidcClientInitiatedLogoutSuccessHandler logoutHandler = new OidcClientInitiatedLogoutSuccessHandler(clientRegistrationRepository);
-        logoutHandler.setPostLogoutRedirectUri(URI.create("http://localhost:8080/"));
+        logoutHandler.setPostLogoutRedirectUri(URI.create("http://localhost:5000/"));
         return logoutHandler;
     }
 
